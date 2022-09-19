9daf89d0faaa2d035193e6835839e304fb21a64f
kthoangweb <113091530+kthoangweb@users.noreply.github.com>
2022-09-19 22:14:21 +0700

    Update postdeploy.sh

--- bin/scripts/postdeploy.sh
+++ bin/scripts/postdeploy.sh
@@ -11,6 +11,11 @@ if [ ! -z "$HEROKU_APP_NAME" ]; then
 	wp_password=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)
 	wp_email="github@philippheuer.me"
 
+	# install pg4wp
+	git clone https://github.com/kevinoid/postgresql-for-wordpress.git /tmp/
+	mv /tmp/postgresql-for-wordpress/pg4wp $HOME/web/wp/wp-content/
+	cp $HOME/web/wp/wp-content/pg4wp/db.php $HOME/web/wp/wp-content/db.php
+
 	# wordpress: install
 	vendor/bin/wp core install --url="$wp_url" --title="$wp_title" --admin_user="$wp_user" --admin_password="$wp_password" --admin_email="$wp_email"
 fi
